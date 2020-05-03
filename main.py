1b8c3b65c3d370a8db5138450d9e29a1ecda6ab0
loslio <64747557+loslio@users.noreply.github.com>
2020-05-03 17:59:59 -0500

    Scraping a url digitada

---
+++ main.py
@@ -0,0 +1,10 @@
+url = input('Ingrese url a realizar el scraping: ')
+
+print('''
+Realizando scraping...
+Esta operacion puede durar unos minutos...
+Loading[####.....]
+Completado con exito
+''')
+
+print(f'A la siguiente URL: {url} se le realizo un scraping')
